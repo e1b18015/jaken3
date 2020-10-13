9d3c1fcc27082e6fd57d523d20247c1a695e9282
e1b18015 <e1b18015@oit.ac.jp>
2020-10-13 15:01:32 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z0950/kaizi/jaken3/Jaken3Application.java
@@ -0,0 +1,13 @@
+package oit.is.z0950.kaizi.jaken3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaken3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaken3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0950/kaizi/jaken3/Jaken3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0950.kaizi.jaken3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaken3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
